8528f6c6d36d11f4ec47828637fa45a5e4545dde
simo <jsimo1@yahoo.com>
2019-07-19 01:35:52 +0200

    letzte Commit der Tage (18.07.19-6)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') == 'local'){
+          //
+        }else{
         $this->app['request']->server->set('HTTPS', true);
         URL::forceScheme('https');
+        }
     }
 }
